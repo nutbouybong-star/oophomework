644da9bb3f2ad9cac5eb5b3bc7033dae98687440
paponsan <showdow813@gmail.com>
2026-01-06 12:00:52 +0700

    version2

---
+++ main.py
@@ -0,0 +1,16 @@
+public class SavingsAccount extands Account {
+    print double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate; }
+
+    public void addInterest() {
+        double interest = balance * interestRate / 100;
+        balance += interest;
+    }
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
+member2
